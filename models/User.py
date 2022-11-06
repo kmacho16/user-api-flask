42f7e72e57d72a928b065a5fa48e0fc7de0f4fe8
Juan Camacho <jccamfer@gmail.com>
2022-11-05 23:33:46 -0500

    dev: add models to project

---
+++ models/User.py
@@ -0,0 +1,9 @@
+from ..config import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    typeDocument = db.Column(db.String(50))
+    numDocument = db.Column(db.String(80))
+    name = db.Column(db.String(200))
+    lastaName = db.Column(db.String(200))
+    hobbie = db.Column(db.String(200))
