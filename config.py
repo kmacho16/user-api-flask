58cf745436b31b1a8653a7570309abad185792d0
Juan Camacho <jccamfer@gmail.com>
2022-11-05 23:28:12 -0500

    dev: add config DB file to project

---
+++ config.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://flask:flask123@localhost/users' 
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+ 
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
