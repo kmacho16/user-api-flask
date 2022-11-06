deb503bc2695d6bf955bc7681d8126b7eef63129
Juan Camacho <jccamfer@gmail.com>
2022-11-06 11:16:09 -0500

    dev: setup migration done

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://flask:flask123@localhost/users' 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://flask:flask123@127.0.0.1:8889/users' 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
  
 db = SQLAlchemy(app)
