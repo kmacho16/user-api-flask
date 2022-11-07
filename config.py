8e8e77d658f73ed516823f1905b50d173972bf51
Juan Camacho <jccamfer@gmail.com>
2022-11-07 14:26:03 -0500

    update conection DB

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://flask:JZoyvG4eCXOsy2QgQKzgbOhRCHK4hcSz@dpg-cdkl9fsgqg43pc43eu40-a/users_vb61'#'postgresql://flask:JZoyvG4eCXOsy2QgQKzgbOhRCHK4hcSz@dpg-cdkl9fsgqg43pc43eu40-a.oregon-postgres.render.com/users_vb61' #'mysql://flask:flask123@127.0.0.1:8889/users' 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://flask:JZoyvG4eCXOsy2QgQKzgbOhRCHK4hcSz@dpg-cdkl9fsgqg43pc43eu40-a/users_vb61'#'postgresql://flask:JZoyvG4eCXOsy2QgQKzgbOhRCHK4hcSz@dpg-cdkl9fsgqg43pc43eu40-a.oregon-postgres.render.com/users_vb61' #'mysql://flask:flask123@127.0.0.1:8889/users' 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
  
 db = SQLAlchemy(app)
